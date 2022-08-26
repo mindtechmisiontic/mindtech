79e1fa4058297235f60dd037b560bcd58e11674d
afbernalh94 <afbernalh@gmail.com>
2022-08-25 20:43:12 -0500

    Creacion de entidades

--- src/main/java/entidades/Transaction.java
+++ src/main/java/entidades/Transaction.java
@@ -1,4 +1,17 @@
 package entidades;
-
+import  javax.persistence.*;
+@Entity
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String concept;
+    private float amount;
+    @ManyToOne
+    @JoinColumn(name="employee_id")
+    private Employee user;
+    @ManyToOne
+    @JoinColumn(name="enterprise_id")
+    private Enterprise enterprise;
+    public Transaction(){}
 }
